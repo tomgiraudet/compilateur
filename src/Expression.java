2ff709e6a370ef5e71c024ec783bad56f0d19217
YvonnigJan <YvonnigJan@users.noreply.github.com>
2015-03-06 09:03:44 +0100

    Update and rename src to src/Expression.java

---
+++ src/Expression.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+public class Expression {
+	Stack<String> types = new Stack<String>();
+	Stack<Character> operations = new Stack<Character>();
+	String[]
+	
+	
+	void ajoutType(String type){
+		types.push(type);
+	}
+	
+	void ajoutOp(char op){
+		operations.push(op)
+	}
+	
+	boolean testPlus(String type1)
+}
