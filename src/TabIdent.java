000d895027d48ea9d14ad68c9736e596ca60d589
Florent THOMAS-MOREL <florentthomasmorel@who-wanna.com>
2015-03-06 08:39:40 +0100

    premier commit TabIdent

--- src/TabIdent.java
+++ src/TabIdent.java
@@ -1,4 +1,24 @@
+import java.util.HashMap;
 
 public class TabIdent {
 
+	int taille;
+	private HashMap<String,Ident> table;
+	
+	public TabIdent(int taille){
+		this.taille = taille;
+		
+	}
+	
+	public Ident chercheIdent(String clef){
+		
+	}
+	
+	public boolean existeIdent(String clef){
+		
+	}
+	
+	public void rangeIdent(String clef, Ident id){
+		
+	}
 }
