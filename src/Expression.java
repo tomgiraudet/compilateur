8a45c8967fd753ed8a518989c47f5ad8effab852
YvonnigJan <YvonnigJan@users.noreply.github.com>
2015-03-06 10:07:03 +0100

    Update Expression.java

--- src/Expression.java
+++ src/Expression.java
@@ -1,15 +1,15 @@
 import java.util.Stack;
 
 public class Expression {
-	Stack<String> types = new Stack<String>();
-	Stack<Character> operations = new Stack<Character>();
+	Stack<Type> types = new Stack<Type>();
+	Stack<Op> operations = new Stack<Op>();
 	
 	
 	void ajoutType(Type t){
 		types.push(t);
 	}
 	
-	void ajoutOp(char op){
+	void ajoutOp(Op op){
 		operations.push(op);
 	}
 	
@@ -50,4 +50,33 @@ public class Expression {
 		}
 		return Type.ERROR;
 	}
+	Type testStacks(){
+		while(operations.peek() != NULL){
+			switch(operations.pop()){
+			case Op.PLUS :
+			case Op.MOINS :
+			case Op.MUL :
+			case Op.DIV :
+				types.push(testAddSubMulDiv(types.pop(), types.pop()));
+			break;
+			case Op.INF :
+			case Op.SUP :
+			case Op.INFE :
+			case Op.SUPE :
+				types.push(testSupInf(types.pop(), types.pop()));
+			break;
+			case Op.EQU :
+			case Op.DIFF :
+				types.push(testEQU(types.pop(), types.pop()));
+			break;
+			case Op.OR :
+			case Op.AND :
+				types.push(testAndOr(types.pop(), types.pop()));
+			break;
+			default :
+				types.push(Type.ERROR);
+			}
+		}
+	}
+	
 }
