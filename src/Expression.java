ffbe417b22c20d7cccc1d9984a91ea41f380d00c
YvonnigJan <YvonnigJan@users.noreply.github.com>
2015-03-06 09:31:32 +0100

    Update Expression.java

--- src/Expression.java
+++ src/Expression.java
@@ -3,16 +3,51 @@ import java.util.Stack;
 public class Expression {
 	Stack<String> types = new Stack<String>();
 	Stack<Character> operations = new Stack<Character>();
-	String[] typesAutorises = new String[]
 	
 	
-	void ajoutType(String type){
-		types.push(type);
+	void ajoutType(Type t){
+		types.push(t);
 	}
 	
 	void ajoutOp(char op){
-		operations.push(op)
+		operations.push(op);
 	}
 	
-	boolean testPlus(String type1)
+	Type testAddSubMulDiv(Type type1, Type type2){
+		if(type1 == Type.INT){
+			if(type2 == Type.INT){
+				return Type.INT;
+			}
+		}
+		return Type.ERROR;
+	}
+	Type testSupInf(Type type1, Type type2){
+		if(type1 == Type.INT){
+			if(type2 == Type.INT){
+				return Type.BOOL;
+			}
+		}
+		return Type.ERROR;
+	}
+	Type testEQU(Type type1, Type type2){
+		if(type1 == Type.INT){
+			if(type2 == Type.INT){
+				return Type.BOOL;
+			}
+		}
+		if(type1 == Type.BOOL){
+			if(type2 == Type.BOOL){
+				return Type.BOOL;
+			}
+		}
+		return Type.ERROR;
+	}
+	Type testAndOr(Type type1, Type type2){
+		if(type1 == Type.BOOL){
+			if(type2 == Type.BOOL){
+				return Type.BOOL;
+			}
+		}
+		return Type.ERROR;
+	}
 }
