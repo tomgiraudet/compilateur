87b88365bdebdca49b79872a772926da046447e0
TomGiraudet <tom.giraudet@insa-rennes.fr>
2015-03-09 14:56:47 +0100

    Ajout IdParam, Declaration

---
+++ src/Declaration.java
@@ -0,0 +1,18 @@
+
+public class Declaration {
+	
+	protected int offset;
+	
+
+	
+	// lit une variable ou une constante et son type (+ sa valeur si c'est une constante)
+	// et ça la range avec un certain offset si c'est une variable ou sa valeur si c'est une constante dans TabIdent.
+	
+	public Declaration() {
+		this.offset = -2;
+	}
+	
+	public void DefVariable(Type _type)
+	public void DefConstante(String nom)
+	
+}
