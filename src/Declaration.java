b2e787d3e092e1b5b11fa8338791afa15536286c
TomGiraudet <tom.giraudet@insa-rennes.fr>
2015-03-09 15:33:31 +0100

    avancée classe déclaration

--- src/Declaration.java
+++ src/Declaration.java
@@ -2,6 +2,8 @@
 public class Declaration {
 	
 	protected int offset;
+	protected Type lastType; // Utile pour des déclarations du type public int a,b,c,d
+	protected String lastConstanteName;
 	
 
 	
@@ -12,7 +14,37 @@ public class Declaration {
 		this.offset = -2;
 	}
 	
-	public void DefVariable(Type _type)
-	public void DefConstante(String nom)
+	// Variables : 
+	public void defTypeVariable(Type _type){
+		lastType = _type;
+	}
+	public void defVariable(String _nom){
+		Yaka.tabIdent.rangeIdent(_nom, new IdVar(lastType, offset));
+		offset -= 2;
+		// Penser à rajouter l'erreur si la variable est déjà présente dans la table (If + erreur)
+	}
+	
+	/*public void setVariable(String _nom, int _valeur){
+		if(Yaka.tabIdent.existeIdent(_nom)){
+			Ident clef = Yaka.tabIdent.rechercheIdent(_nom);
+			
+			
+		}// Penser au else pour l'envoie de l'erreur
+		
+	}*/
+	
+	// Constantes :
+	public void defConstante(String _nom){
+		lastConstanteName = _nom;
+	}
+	public void setConstante(Type _type, int _valeur){
+		Yaka.TabIdent.rangeIdent(lastConstanteName, new IdConst(_type, offset));
+		offset -=2;
+		// Penser à rajouter l'erreur si la constante est déjà présente dans la table (If + erreur)
+	}
+	public void setConstante(String _ident){}
+	
+	
+	
 	
 }
