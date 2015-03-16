82f35a939cc4cde8046bc2c72592f0c5d2f798c6
TomGiraudet <tom.giraudet@insa-rennes.fr>
2015-03-16 11:10:43 +0100

    TOM

---
+++ src/ErrorManager.java
@@ -0,0 +1,19 @@
+
+public class ErrorManager {
+	
+	static String NOT_CONSTANT_OR_VARIABLE 	= " isn't a constant or a variable";
+	static String IDENT_DOESNT_EXIST		= " has not been declared";
+	static String MISMATCH_TYPES			= " types don't match at the affectation";
+	static String ERROR_IN_EXPRESSION 		= " error in expression";
+	static String TYPE_ISSUE				= " type issue";
+	static String WRONG_OPERATOR			= " wrong operator";
+	static String IDENT_ALREADY_EXISTS		= " already exists";
+	static String YVO_NOT_PRESENT			= " Yvo va arriver bient™t";
+	
+	public static void ErrorDeclaration(int _lineError, String _identError, String _typeError){
+		System.out.println("Line " + _lineError + " : " + _identError + _typeError);
+	}
+	public static void ErrorDeclaration(int _lineError, String _typeError){
+		System.out.println("Line " + _lineError + " :"+ _typeError);
+	}
+}
