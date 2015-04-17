d56677f545acb600bc736de4bd19a00c44314fd7
Juliette Gonzalez <juliette.gonzalez@outlook.com>
2015-04-17 17:48:03 +0200

    ju

--- src/TabIdent.java
+++ src/TabIdent.java
@@ -3,20 +3,39 @@ import java.util.HashMap;
 public class TabIdent {
 
 	private HashMap<String,Ident> table;
+	private HashMap<String, Ident> tableLocale;
 	
 	public TabIdent(){
 		table = new HashMap<String,Ident>();
+		tableLocale = new HashMap<String,Ident>();
 	}
 	
 	public Ident chercheIdent(String clef){
 		return table.get(clef);
 	}
 	
+	public Ident chercheIdentLocale(String clef){
+		return tableLocale.get(clef);
+	}
+	
 	public boolean existeIdent(String clef){
 		return table.containsKey(clef);
+	}
+	
+	public boolean existeIdentLocal(String clef){
+		return tableLocale.containsKey(clef);
 	}
 	
 	public void rangeIdent(String clef, Ident id){
 		table.put(clef, id);
 	}
+	
+	public void rangeIdentLocal(String clef, Ident id){
+		tableLocale.put(clef, id);
+	}
+	
+	public void clear(){
+		tableLocale.clear();
+	}
+	
 }
